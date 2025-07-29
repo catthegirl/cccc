7cc535477909bb58156d4d78f94cbbc092163d90
= <=>
2025-07-29 18:56:21 -0400

    Add basic CCC to MKS conversions

--- cccc.c
+++ cccc.c
@@ -6,7 +6,7 @@
 #define MIDDLE_C 261.6256
 #define CAL 4184.0
 
-//this file includes functions for converting ccc units to metric units
+//this file includes functions for converting ccc units to metric units and vice versa
 
 double meter_To_C_per_Middle_C(double meters){
     return meters*(LIGHTSPEED/MIDDLE_C);
@@ -16,4 +16,13 @@ double sec_To_Per_Middle_C(double seconds){
 }
 double kg_To_Cal_Per_C_Squared(double killograms){
     return killograms*(CAL/(LIGHTSPEED*LIGHTSPEED));
+}
+double C_Per_Middle_C_To_Meters(double C_Per_Middle_Cs){
+    return C_Per_Middle_Cs*(MIDDLE_C/LIGHTSPEED);
+}
+double Per_Midle_C_To_Secs(double Per_Middle_Cs){
+    return Per_Middle_Cs*MIDDLE_C;
+}
+double Cal_Per_C_Squared_To_Kg(double Cal_Per_C_Squared){
+    return Cal_Per_C_Squared*((LIGHTSPEED*LIGHTSPEED)/CAL)
 }
