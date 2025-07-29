1f185149048a8a046647e8ba49552ad762d0453b
= <=>
2025-07-29 18:57:45 -0400

    Enforce consistent function naming schema

--- cccc.c
+++ cccc.c
@@ -8,13 +8,13 @@
 
 //this file includes functions for converting ccc units to metric units and vice versa
 
-double meter_To_C_per_Middle_C(double meters){
+double Meter_To_C_Per_Middle_C(double meters){
     return meters*(LIGHTSPEED/MIDDLE_C);
 }
-double sec_To_Per_Middle_C(double seconds){
+double Sec_To_Per_Middle_C(double seconds){
     return seconds*(1/MIDDLE_C);
 }
-double kg_To_Cal_Per_C_Squared(double killograms){
+double Kg_To_Cal_Per_C_Squared(double killograms){
     return killograms*(CAL/(LIGHTSPEED*LIGHTSPEED));
 }
 double C_Per_Middle_C_To_Meters(double C_Per_Middle_Cs){
