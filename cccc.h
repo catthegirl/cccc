f42a23d4a7f35808bc0f13a8f920c56d80e084ae
= <=>
2025-08-01 14:44:26 -0400

    create public header

---
+++ cccc.h
@@ -0,0 +1,16 @@
+#ifndef CCCC_PRIMARY_HEADER
+
+#define CCCC_PRIMARY_HEADER
+
+double Meter_To_C_Per_Middle_C(double meters);
+double Sec_To_Per_Middle_C(double seconds);
+double Kg_To_Cal_Per_C_Squared(double killograms);
+double C_Per_Middle_C_To_Meter(double C_Per_Middle_C);
+double Per_Middle_C_To_Sec(double Per_Middle_C);
+double Cal_Per_C_Squared_To_Kg(double Cal_Per_C_Squared);
+
+#define LIGHTSPEED_MPS 299792458.0
+#define MIDDLE_C_SEC 261.6256
+#define CAL_JOULE 4184.0
+
+#endif
